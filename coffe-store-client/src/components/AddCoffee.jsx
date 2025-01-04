3c2fdfd6e9b346bcee6610a02c08292ff2bda333
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-01-04 18:59:26 +0600

    form added

--- coffe-store-client/src/components/AddCoffee.jsx
+++ coffe-store-client/src/components/AddCoffee.jsx
@@ -1,7 +1,32 @@
 const AddCoffee = () => {
   return (
-    <div>
-      <h2>Add Coffee</h2>
+    <div className="bg-[#f4f3f0] p-24 text-black">
+      <h2 className="text-3xl font-extrabold">Add Coffee</h2>
+      <form>
+        <div className="md:flex gap-5 ">
+          <div className="join">
+            <button className="btn join-item rounded-r-full">
+              Coffee Name
+            </button>
+            <input
+              type="text"
+              className="input input-bordered join-item"
+              placeholder="Coffee Name"
+            />
+          </div>
+
+          <div className="join">
+            <button className="btn join-item rounded-r-full">
+              Available Quantity
+            </button>
+            <input
+              type="number"
+              className="input input-bordered join-item"
+              placeholder="Available Quantity"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
--- coffe-store-server/index.js
+++ coffe-store-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,10 +12,7 @@ app.use(express.json());
 // CoffeeMaster
 // RKH39xkIF2NQ9UP9
 
-// const uri =
-//   "mongodb+srv://<db_username>:<db_password>@cluster0.kbm4w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri =
-  "mongodb+srv://CoffeeMaster:RKH39xkIF2NQ9UP9@cluster0.kbm4w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbm4w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +27,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -36,7 +35,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
