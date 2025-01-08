602f15edca7b1d9b8f2c63618d2fb4fcbe95d02b
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-01-08 23:30:22 +0600

    completed

--- coffe-store-client/src/components/CoffeeCard.jsx
+++ coffe-store-client/src/components/CoffeeCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
-const CoffeeCard = ({ coffee }) => {
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
   const { _id, name, chef, supplier, taste, category, details, photo } = coffee;
 
   const handleDelete = (_id) => {
@@ -29,6 +29,8 @@ const CoffeeCard = ({ coffee }) => {
                 text: "Your Coffee has been deleted.",
                 icon: "success",
               });
+              const remaining = coffees.filter((cof) => cof._id != id);
+              setCoffees(remaining);
             }
           });
       }
